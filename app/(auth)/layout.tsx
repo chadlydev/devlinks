d0ceaa6b402a58813564494da9208ccffe8bbb77
ch4dly <github.refute247@8alias.com>
2024-05-19 15:57:16 +0200

    Update responsive design class in AuthLayout component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import Logo from '@/components/logo';
 
 export default async function AuthLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className='flex min-h-svh flex-col gap-16 px-4 pt-8 md:items-center md:justify-center md:gap-8 md:pt-0'>
+		<div className='flex min-h-svh flex-col gap-16 px-4 pt-8 sm:items-center sm:justify-center sm:gap-8 sm:pt-0'>
 			<Logo />
 			{children}
 		</div>
