c87f5997690f1aee3fbf7805605d14e500e6c23b
ch4dly <github.refute247@8alias.com>
2024-05-21 10:31:23 +0200

    Refactor auth layout for header section

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,8 +3,10 @@ import Logo from '@/components/logo';
 
 export default async function AuthLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className='flex min-h-svh flex-col gap-16 px-4 pt-8 sm:items-center sm:justify-center sm:gap-8 sm:pt-0'>
-			<Logo />
+		<div className='flex min-h-svh flex-col gap-16 px-4 sm:items-center sm:justify-center sm:gap-8 sm:pt-0'>
+			<header className='flex h-24 items-center sm:h-fit'>
+				<Logo />
+			</header>
 			{children}
 		</div>
 	);
