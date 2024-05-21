5c7fafa00917dc527797cc9ec28214b4d242e212
ch4dly <github.refute247@8alias.com>
2024-05-21 16:09:58 +0200

    Added phone mockup svg file and updated LinksPage layout

--- app/(app)/app/links/page.tsx
+++ app/(app)/app/links/page.tsx
@@ -1,10 +1,16 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { PlusIcon } from '@/components/icons';
+import EmptyState from '@/app/(app)/app/links/empty-state';
+import Image from 'next/image';
+import phoneMockup from '@/public/illustration-phone-mockup.svg';
 
 export default function LinksPage() {
 	return (
-		<main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+		<main className='flex p-4 lg:gap-6 lg:p-6'>
+			<div className='bg-card hidden w-[480px] flex-shrink-0 flex-grow-0 items-center justify-center rounded-lg border p-6 px-12 shadow-sm lg:flex'>
+				<Image src={phoneMockup} alt='phone mockup' />
+			</div>
 			<Card>
 				<CardHeader>
 					<CardTitle>Customize your links</CardTitle>
@@ -12,11 +18,16 @@ export default function LinksPage() {
 						Add/edit/remove links below and then share all your profiles with the world!
 					</CardDescription>
 				</CardHeader>
-				<CardContent className='flex flex-col'>
+				<CardContent className='flex flex-col gap-8'>
 					<Button variant='secondary'>
 						<PlusIcon size={16} />
 						Add new link
 					</Button>
+
+					{/*	TODO add Empty state */}
+					<EmptyState />
+
+					{/*	TODO add Link form */}
 				</CardContent>
 			</Card>
 		</main>
