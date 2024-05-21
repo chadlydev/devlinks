5cfd7b76c923d1d14f8f0fe0a1f65256a578ad5b
ch4dly <github.refute247@8alias.com>
2024-05-21 13:11:36 +0200

    Enhanced LinksPage component with additional features

--- app/(app)/app/links/page.tsx
+++ app/(app)/app/links/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-	return <div>links</div>;
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { PlusIcon } from '@/components/icons';
+
+export default function LinksPage() {
+	return (
+		<main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+			<Card>
+				<CardHeader>
+					<CardTitle>Customize your links</CardTitle>
+					<CardDescription>
+						Add/edit/remove links below and then share all your profiles with the world!
+					</CardDescription>
+				</CardHeader>
+				<CardContent className='flex flex-col'>
+					<Button variant='secondary'>
+						<PlusIcon size={16} />
+						Add new link
+					</Button>
+				</CardContent>
+			</Card>
+		</main>
+	);
 }
