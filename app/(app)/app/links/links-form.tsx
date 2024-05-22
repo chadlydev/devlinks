2eaea043825c81941cf9d0c3624179bc2c7c28a5
ch4dly <github.refute247@8alias.com>
2024-05-22 18:22:20 +0200

    Added ScrollArea component to links form

--- app/(app)/app/links/links-form.tsx
+++ app/(app)/app/links/links-form.tsx
@@ -10,6 +10,7 @@ import EmptyState from '@/app/(app)/app/links/empty-state';
 import { Large } from '@/components/typography';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { cn } from '@/lib/utils';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const platformOptions = [
 	{ value: 'github', label: 'GitHub' },
@@ -77,84 +78,94 @@ export default function LinksForm() {
 				<EmptyState />
 			) : (
 				<Form {...form}>
-					<form onSubmit={form.handleSubmit(onSubmit)} className='flex h-full flex-col gap-4'>
-						{fields.map((field, index) => {
-							return (
-								<div key={field.id} className={cn('bg-muted flex gap-4 rounded-lg p-4 pb-6 pt-4')}>
-									<div className='flex flex-grow flex-col gap-4'>
-										<div className='flex items-center justify-between'>
-											<Large>Link #{index + 1}</Large>
-										</div>
-										<FormField
-											control={control}
-											name={`links.${index}.platform` as const}
-											render={({ field }) => (
-												<FormItem>
-													<FormLabel>Platform</FormLabel>
-													<Select onValueChange={field.onChange} defaultValue={field.value}>
-														<FormControl>
-															<SelectTrigger>
-																<SelectValue placeholder='Select Platform' />
-															</SelectTrigger>
-														</FormControl>
-														<SelectContent>
-															{platformOptions.map((platformOption) => (
-																<SelectItem value={platformOption.value} key={platformOption.value}>
-																	{platformOption.label}
-																</SelectItem>
-															))}
-														</SelectContent>
-													</Select>
-													<FormMessage />
-												</FormItem>
-											)}
-										/>
-										<FormField
-											control={control}
-											name={`links.${index}.link` as const}
-											render={({ field }) => (
-												<FormItem>
-													<FormLabel>Link</FormLabel>
-													<FormControl>
-														<FormInput {...field} placeholder='https://github.com/chadlydev' />
-													</FormControl>
-													<FormMessage />
-												</FormItem>
-											)}
-										/>
-									</div>
-									<div className='flex flex-col gap-4'>
-										<Button
-											type='button'
-											size='icon'
-											variant='outline'
-											disabled={index === 0}
-											onClick={() => handleMoveUp(index)}
+					<form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-grow flex-col gap-4'>
+						<ScrollArea className='-mr-4 h-[calc(100svh-376px)] pr-4 md:h-[calc(100svh-404px)]'>
+							<ol className='flex h-max flex-col gap-4'>
+								{fields.map((field, index) => {
+									return (
+										<li
+											key={field.id}
+											className={cn('bg-muted flex gap-4 rounded-lg p-4 pb-6 pt-4')}
 										>
-											<ChevronUpIcon size={16} />
-										</Button>
-										<Button
-											type='button'
-											size='icon'
-											variant='outline'
-											disabled={index + 1 >= fields.length}
-											onClick={() => handleMoveDown(index)}
-										>
-											<ChevronDownIcon size={16} />
-										</Button>
-										<Button
-											type='button'
-											size='icon'
-											variant='destructive'
-											className='mt-auto'
-											onClick={() => remove(index)}
-										>
-											<TrashIcon size={14} />
-										</Button>
-									</div>
-								</div>
-							);
-						})}
+											<div className='flex flex-grow flex-col gap-4'>
+												<div className='flex items-center justify-between'>
+													<Large>Link #{index + 1}</Large>
+												</div>
+												<FormField
+													control={control}
+													name={`links.${index}.platform` as const}
+													render={({ field }) => (
+														<FormItem>
+															<FormLabel>Platform</FormLabel>
+															<Select onValueChange={field.onChange} defaultValue={field.value}>
+																<FormControl>
+																	<SelectTrigger>
+																		<SelectValue placeholder='Select Platform' />
+																	</SelectTrigger>
+																</FormControl>
+																<SelectContent>
+																	{platformOptions.map((platformOption) => (
+																		<SelectItem
+																			value={platformOption.value}
+																			key={platformOption.value}
+																		>
+																			{platformOption.label}
+																		</SelectItem>
+																	))}
+																</SelectContent>
+															</Select>
+															<FormMessage />
+														</FormItem>
+													)}
+												/>
+												<FormField
+													control={control}
+													name={`links.${index}.link` as const}
+													render={({ field }) => (
+														<FormItem>
+															<FormLabel>Link</FormLabel>
+															<FormControl>
+																<FormInput {...field} placeholder='https://github.com/chadlydev' />
+															</FormControl>
+															<FormMessage />
+														</FormItem>
+													)}
+												/>
+											</div>
+											<div className='flex flex-col gap-4'>
+												<Button
+													type='button'
+													size='icon'
+													variant='outline'
+													disabled={index === 0}
+													onClick={() => handleMoveUp(index)}
+												>
+													<ChevronUpIcon size={16} />
+												</Button>
+												<Button
+													type='button'
+													size='icon'
+													variant='outline'
+													disabled={index + 1 >= fields.length}
+													onClick={() => handleMoveDown(index)}
+												>
+													<ChevronDownIcon size={16} />
+												</Button>
+												<Button
+													type='button'
+													size='icon'
+													variant='destructive'
+													className='mt-auto'
+													onClick={() => remove(index)}
+												>
+													<TrashIcon size={14} />
+												</Button>
+											</div>
+										</li>
+									);
+								})}
+							</ol>
+						</ScrollArea>
 
 						<Button disabled={isSubmitting || !isDirty} className='mt-auto'>
 							Save
@@ -165,70 +176,3 @@ export default function LinksForm() {
 		</div>
 	);
 }
-
-// type LinkItemProps = {
-// 	index: number;
-// 	control: Control<TLinksForm>;
-// 	remove: UseFieldArrayRemove;
-// };
-//
-// function LinkItem({ control, remove, index }: LinkItemProps) {
-// 	return (
-// 		<div className={cn('bg-muted flex gap-4 rounded-lg p-4 pb-6 pt-4')}>
-// 			<div className='flex flex-col justify-around'>
-// 				<Button size='icon' variant='outline' disabled={index === 0}>
-// 					<ChevronUpIcon size={16} />
-// 				</Button>
-// 				<Button size='icon' variant='outline'>
-// 					<ChevronDownIcon size={16} />
-// 				</Button>
-// 			</div>
-// 			<div className='flex flex-grow flex-col gap-4'>
-// 				<div className='flex items-center justify-between'>
-// 					<Large>Link #{index + 1}</Large>
-// 					<Button type='button' size='sm' variant='outline' onClick={() => remove(index)}>
-// 						<TrashIcon size={14} />
-// 						Remove
-// 					</Button>
-// 				</div>
-// 				<FormField
-// 					control={control}
-// 					name={`links.${index}.platform` as const}
-// 					render={({ field }) => (
-// 						<FormItem>
-// 							<FormLabel>Platform</FormLabel>
-// 							<Select onValueChange={field.onChange} defaultValue={field.value}>
-// 								<FormControl>
-// 									<SelectTrigger>
-// 										<SelectValue placeholder='Select Platform' />
-// 									</SelectTrigger>
-// 								</FormControl>
-// 								<SelectContent>
-// 									{platformOptions.map((platformOption) => (
-// 										<SelectItem value={platformOption.value} key={platformOption.value}>
-// 											{platformOption.label}
-// 										</SelectItem>
-// 									))}
-// 								</SelectContent>
-// 							</Select>
-// 							<FormMessage />
-// 						</FormItem>
-// 					)}
-// 				/>
-// 				<FormField
-// 					control={control}
-// 					name={`links.${index}.link` as const}
-// 					render={({ field }) => (
-// 						<FormItem>
-// 							<FormLabel>Link</FormLabel>
-// 							<FormControl>
-// 								<FormInput {...field} placeholder='https://github.com/chadlydev' />
-// 							</FormControl>
-// 							<FormMessage />
-// 						</FormItem>
-// 					)}
-// 				/>
-// 			</div>
-// 		</div>
-// 	);
-// }
