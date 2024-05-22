7b576310f7fb9c1652a2913057d793b88205d4c0
ch4dly <github.refute247@8alias.com>
2024-05-22 18:22:25 +0200

    Update layout and styling of LinksPage

--- app/(app)/app/links/page.tsx
+++ app/(app)/app/links/page.tsx
@@ -4,11 +4,11 @@ import LinksForm from '@/app/(app)/app/links/links-form';
 
 export default function LinksPage() {
 	return (
-		<main className='flex p-4 lg:gap-6 lg:p-6'>
-			<div className='bg-card hidden w-[480px] flex-shrink-0 flex-grow-0 items-center justify-center rounded-lg border p-6 px-12 shadow-sm lg:flex'>
+		<main className='flex h-[calc(100svh-64px)] flex-grow p-4 md:h-[calc(100svh-112px)] lg:h-fit lg:gap-6 lg:p-6'>
+			<div className='bg-card fixed hidden h-[calc(100svh-144px)] w-[480px] flex-shrink-0 flex-grow-0 items-center justify-center rounded-lg border p-6 px-12 shadow-sm lg:flex'>
 				<IllustrationPhoneMockup />
 			</div>
-			<Card className='flex flex-grow flex-col'>
+			<Card className='flex h-full flex-grow flex-col lg:ml-[504px]'>
 				<CardHeader>
 					<CardTitle>Customize your links</CardTitle>
 					<CardDescription>
