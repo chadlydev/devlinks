b0e062d48a549c2c8525ad398c194f15ab922572
ch4dly <github.refute247@8alias.com>
2024-05-23 17:23:42 +0200

    Replace XIcon with CrossIcon in dialog and sheet components

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -5,7 +5,7 @@ import * as SheetPrimitive from '@radix-ui/react-dialog';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
-import { XIcon } from '@/components/icons';
+import { CrossIcon } from '@/components/icons';
 
 const Sheet = SheetPrimitive.Root;
 
@@ -61,7 +61,7 @@ const SheetContent = React.forwardRef<
 		<SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
 			{children}
 			<SheetPrimitive.Close className='ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none'>
-				<XIcon size={16} />
+				<CrossIcon size={16} />
 				<span className='sr-only'>Close</span>
 			</SheetPrimitive.Close>
 		</SheetPrimitive.Content>
