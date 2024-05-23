19ca1a9baf438a108a20e133719404fe75ae33a3
ch4dly <github.refute247@8alias.com>
2024-05-23 10:24:17 +0200

    Abstract LinkItem into own dedicated component

---
+++ app/(app)/app/links/link-item.tsx
@@ -0,0 +1,174 @@
+import { cn } from '@/lib/utils';
+import { Large } from '@/components/typography';
+import {
+	FormControl,
+	FormField,
+	FormInput,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from '@/components/ui/form';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import {
+	CheckIcon,
+	ChevronDownIcon,
+	ChevronsUpDownIcon,
+	ChevronUpIcon,
+	TrashIcon
+} from '@/components/icons';
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList
+} from '@/components/ui/command';
+import { UseFieldArrayReturn, UseFormReturn } from 'react-hook-form';
+import { useState } from 'react';
+
+const platformOptions = [
+	{ value: 'github', label: 'GitHub' },
+	{ value: 'twitter', label: 'Twitter' },
+	{ value: 'linkedin', label: 'LinkedIn' },
+	{ value: 'youtube', label: 'YouTube' },
+	{ value: 'facebook', label: 'Facebook' },
+	{ value: 'twitch', label: 'Twitch' },
+	{ value: 'devto', label: 'Dev.to' },
+	{ value: 'codewars', label: 'Codewars' },
+	{ value: 'freecodecamp', label: 'freeCodeCamp' },
+	{ value: 'gitlab', label: 'GitLab' },
+	{ value: 'hashnode', label: 'Hashnode' },
+	{ value: 'stackoverflow', label: 'Stack Overflow' }
+];
+
+type LinkItemProps = {
+	id: string;
+	index: number;
+	form: UseFormReturn<{ links: { platform: string; link: string }[] }, any, undefined>;
+	fieldArray: UseFieldArrayReturn<{ links: { platform: string; link: string }[] }, 'links', 'id'>;
+};
+
+export default function LinkItem({ id, index, form, fieldArray }: LinkItemProps) {
+	const [open, setOpen] = useState(false);
+
+	const handleMoveUp = (index: number) => {
+		fieldArray.swap(index, index - 1);
+	};
+
+	const handleMoveDown = (index: number) => {
+		fieldArray.swap(index, index + 1);
+	};
+
+	return (
+		<li key={id} className={cn('bg-muted flex gap-4 rounded-lg p-4 pb-6 pt-4')}>
+			<div className='flex flex-grow flex-col gap-4'>
+				<div className='flex items-center justify-between'>
+					<Large>Link #{index + 1}</Large>
+				</div>
+				<FormField
+					control={form.control}
+					name={`links.${index}.link` as const}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Link</FormLabel>
+							<FormControl>
+								<FormInput {...field} placeholder='https://github.com/chadlydev' />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name={`links.${index}.platform` as const}
+					render={({ field, fieldState: { error } }) => (
+						<FormItem>
+							<FormLabel>Platform</FormLabel>
+							<Popover open={open} onOpenChange={setOpen}>
+								<PopoverTrigger asChild>
+									<FormControl>
+										<Button
+											variant='outline'
+											role='combobox'
+											className={cn('justify-between font-normal', {
+												'text-muted-foreground': !field.value,
+												'border-destructive': error?.message
+											})}
+										>
+											{field.value
+												? platformOptions.find(
+														(platformOption) => platformOption.value === field.value
+													)?.label
+												: 'Select platform'}
+											<ChevronsUpDownIcon className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+										</Button>
+									</FormControl>
+								</PopoverTrigger>
+								<PopoverContent>
+									<Command>
+										<CommandInput placeholder='Search platform...' />
+										<CommandEmpty>No platform found.</CommandEmpty>
+										<CommandGroup>
+											<CommandList>
+												{platformOptions.map((platformOption) => (
+													<CommandItem
+														value={platformOption.label}
+														key={platformOption.value}
+														onSelect={() => {
+															form.setValue(`links.${index}.platform`, platformOption.value);
+															setOpen(false);
+														}}
+													>
+														<CheckIcon
+															className={cn(
+																'mr-2 h-4 w-4',
+																platformOption.value === field.value ? 'opacity-100' : 'opacity-0'
+															)}
+														/>
+														{platformOption.label}
+													</CommandItem>
+												))}
+											</CommandList>
+										</CommandGroup>
+									</Command>
+								</PopoverContent>
+							</Popover>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+			</div>
+			<div className='flex flex-col gap-4'>
+				<Button
+					type='button'
+					size='icon'
+					variant='outline'
+					disabled={index === 0}
+					onClick={() => handleMoveUp(index)}
+				>
+					<ChevronUpIcon size={16} />
+				</Button>
+				<Button
+					type='button'
+					size='icon'
+					variant='outline'
+					disabled={index + 1 >= fieldArray.fields.length}
+					onClick={() => handleMoveDown(index)}
+				>
+					<ChevronDownIcon size={16} />
+				</Button>
+				<Button
+					type='button'
+					size='icon'
+					variant='outline'
+					className='mt-auto'
+					onClick={() => fieldArray.remove(index)}
+				>
+					<TrashIcon size={14} />
+				</Button>
+			</div>
+		</li>
+	);
+}
--- app/(app)/app/links/links-form.tsx
+++ app/(app)/app/links/links-form.tsx
@@ -4,32 +4,16 @@ import { useFieldArray, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
 import { z } from 'zod';
-import { Form, FormControl, FormField, FormInput, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { CheckIcon, ChevronDownIcon, ChevronsUpDownIcon, ChevronUpIcon, PlusIcon, TrashIcon } from '@/components/icons';
+import { Form } from '@/components/ui/form';
+import { PlusIcon } from '@/components/icons';
 import EmptyState from '@/app/(app)/app/links/empty-state';
-import { Large } from '@/components/typography';
-import { cn } from '@/lib/utils';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
-
-const platformOptions = [
-	{ value: 'github', label: 'GitHub' },
-	{ value: 'twitter', label: 'Twitter' },
-	{ value: 'linkedin', label: 'LinkedIn' },
-	{ value: 'youtube', label: 'YouTube' },
-	{ value: 'facebook', label: 'Facebook' },
-	{ value: 'twitch', label: 'Twitch' },
-	{ value: 'devto', label: 'Dev.to' },
-	{ value: 'codewars', label: 'Codewars' },
-	{ value: 'freecodecamp', label: 'freeCodeCamp' },
-	{ value: 'gitlab', label: 'GitLab' },
-	{ value: 'hashnode', label: 'Hashnode' },
-	{ value: 'stackoverflow', label: 'Stack Overflow' }
-];
+import LinkItem from '@/app/(app)/app/links/link-item';
 
 const linkObject = z.object({
-	platform: z.string({ required_error: 'This field is required' }).min(1),
+	platform: z
+		.string({ required_error: 'This field is required' })
+		.min(1, { message: 'Pick a platform' }),
 	link: z.string({ required_error: 'This field is required' }).url({ message: 'Invalid URL' })
 });
 
@@ -56,18 +40,12 @@ export default function LinksForm() {
 		console.log(values);
 	};
 
-	const { fields, append, remove, swap } = useFieldArray({
+	const fieldArray = useFieldArray({
 		name: 'links',
 		control
 	});
 
-	const handleMoveUp = (index: number) => {
-		swap(index, index - 1);
-	};
-
-	const handleMoveDown = (index: number) => {
-		swap(index, index + 1);
-	};
+	const { append, fields } = fieldArray;
 
 	return (
 		<div className='flex h-full flex-grow flex-col gap-8'>
@@ -84,121 +62,13 @@ export default function LinksForm() {
 							<ol className='flex h-max flex-col gap-4'>
 								{fields.map((field, index) => {
 									return (
-										<li
+										<LinkItem
 											key={field.id}
-											className={cn('bg-muted flex gap-4 rounded-lg p-4 pb-6 pt-4')}
-										>
-											<div className='flex flex-grow flex-col gap-4'>
-												<div className='flex items-center justify-between'>
-													<Large>Link #{index + 1}</Large>
-												</div>
-												<FormField
-													control={control}
-													name={`links.${index}.platform` as const}
-													render={({ field }) => (
-														<FormItem>
-															<FormLabel>Platform</FormLabel>
-															<Popover>
-																<PopoverTrigger asChild>
-																	<FormControl>
-																		<Button
-																			variant='outline'
-																			role='combobox'
-																			className={cn(
-																				'justify-between font-normal',
-																				!field.value && 'text-muted-foreground'
-																			)}
-																		>
-																			{field.value
-																				? platformOptions.find(
-																						(platformOption) => platformOption.value === field.value
-																					)?.label
-																				: 'Select platform'}
-																			<ChevronsUpDownIcon className='ml-2 h-4 w-4 shrink-0 opacity-50' />
-																		</Button>
-																	</FormControl>
-																</PopoverTrigger>
-																<PopoverContent>
-																	<Command>
-																		<CommandInput placeholder='Search platform...' />
-																		<CommandEmpty>No platform found.</CommandEmpty>
-																		<CommandGroup>
-																			<CommandList>
-																				{platformOptions.map((platformOption) => (
-																					<CommandItem
-																						value={platformOption.label}
-																						key={platformOption.value}
-																						onSelect={() => {
-																							form.setValue(
-																								`links.${index}.platform`,
-																								platformOption.value
-																							);
-																						}}
-																					>
-																						<CheckIcon
-																							className={cn(
-																								'mr-2 h-4 w-4',
-																								platformOption.value === field.value
-																									? 'opacity-100'
-																									: 'opacity-0'
-																							)}
-																						/>
-																						{platformOption.label}
-																					</CommandItem>
-																				))}
-																			</CommandList>
-																		</CommandGroup>
-																	</Command>
-																</PopoverContent>
-															</Popover>
-															<FormMessage />
-														</FormItem>
-													)}
-												/>
-												<FormField
-													control={control}
-													name={`links.${index}.link` as const}
-													render={({ field }) => (
-														<FormItem>
-															<FormLabel>Link</FormLabel>
-															<FormControl>
-																<FormInput {...field} placeholder='https://github.com/chadlydev' />
-															</FormControl>
-															<FormMessage />
-														</FormItem>
-													)}
-												/>
-											</div>
-											<div className='flex flex-col gap-4'>
-												<Button
-													type='button'
-													size='icon'
-													variant='outline'
-													disabled={index === 0}
-													onClick={() => handleMoveUp(index)}
-												>
-													<ChevronUpIcon size={16} />
-												</Button>
-												<Button
-													type='button'
-													size='icon'
-													variant='outline'
-													disabled={index + 1 >= fields.length}
-													onClick={() => handleMoveDown(index)}
-												>
-													<ChevronDownIcon size={16} />
-												</Button>
-												<Button
-													type='button'
-													size='icon'
-													variant='outline'
-													className='mt-auto'
-													onClick={() => remove(index)}
-												>
-													<TrashIcon size={14} />
-												</Button>
-											</div>
-										</li>
+											id={field.id}
+											index={index}
+											form={form}
+											fieldArray={fieldArray}
+										/>
 									);
 								})}
 							</ol>
