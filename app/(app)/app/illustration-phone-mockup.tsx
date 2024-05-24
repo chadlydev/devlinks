7434ea947e587eba1f25793d726aaf525f2800c0
ch4dly <github.refute247@8alias.com>
2024-05-24 11:55:04 +0200

    Implement user profile context in app layout and mockup

--- app/(app)/app/illustration-phone-mockup.tsx
+++ app/(app)/app/illustration-phone-mockup.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import { Large, Small } from '@/components/typography';
 import { SocialButton } from '@/components/social-button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { useProfileContext } from '@/contexts/profile-context';
+import { PlatformValue } from '@/lib/types';
 
 export default function IllustrationPhoneMockup() {
+	const { user, links } = useProfileContext();
+
 	return (
 		<div className='relative'>
 			<svg
@@ -21,37 +27,25 @@ export default function IllustrationPhoneMockup() {
 						d='M12 55.5C12 30.923 31.923 11 56.5 11h24C86.851 11 92 16.149 92 22.5c0 8.008 6.492 14.5 14.5 14.5h95c8.008 0 14.5-6.492 14.5-14.5 0-6.351 5.149-11.5 11.5-11.5h24c24.577 0 44.5 19.923 44.5 44.5v521c0 24.577-19.923 44.5-44.5 44.5h-195C31.923 621 12 601.077 12 576.5v-521Z'
 						className='stroke-border fill-card dark:fill-card-foreground/10'
 					/>
-					{/*<path*/}
-					{/*	d='M153.5 160c26.51 0 48-21.49 48-48s-21.49-48-48-48-48 21.49-48 48 21.49 48 48 48Zm72 25h-144a8 8 0 0 0 0 16h144a8 8 0 0 0 0-16Zm-40 29h-64a4 4 0 0 0 0 8h64a4 4 0 0 0 0-8Zm78.5 64H43a8 8 0 0 0-8 8v28a8 8 0 0 0 8 8h221a8 8 0 0 0 8-8v-28a8 8 0 0 0-8-8Zm0 64H43a8 8 0 0 0-8 8v28a8 8 0 0 0 8 8h221a8 8 0 0 0 8-8v-28a8 8 0 0 0-8-8Zm0 64H43a8 8 0 0 0-8 8v28a8 8 0 0 0 8 8h221a8 8 0 0 0 8-8v-28a8 8 0 0 0-8-8Zm0 64H43a8 8 0 0 0-8 8v28a8 8 0 0 0 8 8h221a8 8 0 0 0 8-8v-28a8 8 0 0 0-8-8Zm0 64H43a8 8 0 0 0-8 8v28a8 8 0 0 0 8 8h221a8 8 0 0 0 8-8v-28a8 8 0 0 0-8-8Z'*/}
-					{/*	className='fill-muted'*/}
-					{/*/>*/}
 				</g>
 			</svg>
 
 			<div className='absolute top-0 flex w-[306px] flex-grow flex-col items-center gap-12 px-10 pt-16'>
 				<div className='flex flex-col items-center gap-5'>
 					<Avatar className='bg-card size-24 border'>
-						<AvatarImage src='path_to_image' alt='avatar' />
+						<AvatarImage src={user.profilePictureUrl} alt='avatar' />
 						<AvatarFallback className='text-2xl'>CR</AvatarFallback>
 					</Avatar>
 					<div className='flex flex-col items-center gap-2'>
-						<Large>Chadly Riedewald</Large>
-						<Small>hi@chadly.dev</Small>
+						<Large>{user.name}</Large>
+						<Small>{user.email}</Small>
 					</div>
 				</div>
 
 				<div className='flex w-full flex-col items-center gap-6'>
-					<SocialButton variant='github' />
-					<SocialButton variant='x' />
-					<SocialButton variant='linkedin' />
-					<SocialButton variant='youtube' />
-					<SocialButton variant='twitch' />
-					{/*<SocialButton variant='devto' />*/}
-					{/*<SocialButton variant='codewars' />*/}
-					{/*<SocialButton variant='freecodecamp' />*/}
-					{/*<SocialButton variant='gitlab' />*/}
-					{/*<SocialButton variant='hashnode' />*/}
-					{/*<SocialButton variant='stackoverflow' />*/}
+					{links.slice(0, 5).map((link, index) => (
+						<SocialButton key={index} variant={link.platform as PlatformValue} />
+					))}
 				</div>
 			</div>
 		</div>
--- app/(app)/app/layout.tsx
+++ app/(app)/app/layout.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import Header from '@/app/(app)/app/header';
 import IllustrationPhoneMockup from '@/app/(app)/app/illustration-phone-mockup';
+import ProfileContextProvider from '@/contexts/profile-context';
 
 export default async function AppLayout({ children }: { children: React.ReactNode }) {
+	const data = {
+		user: {
+			name: 'Chadly Riedewald',
+			email: 'hi@chadly.dev',
+			profilePictureUrl: ''
+		},
+		links: [
+			{ platform: 'github', url: '' },
+			{ platform: 'x', url: '' }
+		]
+	};
+
 	return (
 		<div className='flex min-h-svh flex-col'>
 			<Header />
 			<div className='mt-16 md:mt-28'>
-				<div className='flex h-[calc(100svh-64px)] flex-grow p-4 md:h-[calc(100svh-112px)] md:gap-6 md:px-6 md:pb-6 md:pt-0 lg:h-fit'>
-					<div className='bg-card fixed hidden h-[calc(100svh-136px)] w-[480px] flex-shrink-0 flex-grow-0 items-center justify-center rounded-lg border p-6 px-12 shadow-sm lg:flex'>
-						<IllustrationPhoneMockup />
+				<ProfileContextProvider data={data}>
+					<div className='flex h-[calc(100svh-64px)] flex-grow p-4 md:h-[calc(100svh-112px)] md:gap-6 md:px-6 md:pb-6 md:pt-0 lg:h-fit'>
+						<div className='bg-card fixed hidden h-[calc(100svh-136px)] w-[480px] flex-shrink-0 flex-grow-0 items-center justify-center rounded-lg border p-6 px-12 shadow-sm lg:flex'>
+							<IllustrationPhoneMockup />
+						</div>
+						{children}
 					</div>
-					{children}
-				</div>
+				</ProfileContextProvider>
 			</div>
 		</div>
 	);
