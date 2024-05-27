9ba9bd92a2dc55a965aec5b6939d9e2ac6ea0537
ch4dly <github.refute247@8alias.com>
2024-05-27 14:35:20 +0200

    Added new profile details form component

---
+++ app/(app)/app/profile-details/profile-details-form.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormInput,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from '@/components/ui/form';
+
+import React from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@/components/ui/button';
+import { useProfileContext } from '@/contexts/profile-context';
+
+const profileDetailsFormSchema = z.object({
+	name: z.string().max(100),
+	email: z.string().email({ message: 'Please enter a valid email address' }).max(100)
+});
+
+export type TProfileDetailsForm = z.infer<typeof profileDetailsFormSchema>;
+
+export default function ProfileDetailsForm() {
+	const { user } = useProfileContext();
+
+	const form = useForm<TProfileDetailsForm>({
+		resolver: zodResolver(profileDetailsFormSchema),
+		defaultValues: {
+			name: user.name,
+			email: user.email
+		}
+	});
+
+	const {
+		reset,
+		setError,
+		formState: { isSubmitting, isDirty }
+	} = form;
+
+	const onSubmit = (values: TProfileDetailsForm) => {
+		console.log(values);
+	};
+
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className='bg-muted grid gap-4 rounded-lg p-4 pb-6 pt-4'
+			>
+				<FormField
+					control={form.control}
+					name='name'
+					render={({ field }) => (
+						<FormItem className='md:flex md:items-center'>
+							<FormLabel className='md:min-w-60 lg:min-w-80'>Name*</FormLabel>
+							<FormControl>
+								<FormInput placeholder='John Doe' {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='email'
+					render={({ field }) => (
+						<FormItem className='md:flex md:items-center'>
+							<FormLabel className='md:min-w-60 lg:min-w-80'>Email*</FormLabel>
+							<FormControl>
+								<FormInput placeholder='someone@example.com' {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button
+					className='md:ml-auto md:w-[calc(100%-248px)] lg:w-[calc(100%-328px)]'
+					variant='secondary'
+					disabled={isSubmitting || !isDirty}
+				>
+					Save changes
+				</Button>
+			</form>
+		</Form>
+	);
+}
