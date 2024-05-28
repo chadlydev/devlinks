2f3d46b019cfc1b15d784539f82e70ff7aeaf376
ch4dly <github.refute247@8alias.com>
2024-05-28 09:16:17 +0200

    Added database schema and relevant packages

---
+++ db/schema.ts
@@ -0,0 +1,59 @@
+import { boolean, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const userTable = pgTable('user', {
+	id: text('id').primaryKey(),
+	email: text('email').unique(),
+	hashedPassword: text('hashed_password'),
+	emailVerified: boolean('email_verified').notNull().default(false),
+	profilePictureUrl: text('profile_picture_url'),
+	name: text('name')
+});
+
+export const oauthAccountTable = pgTable('oauth_account', {
+	providerUserId: text('provider_user_id').primaryKey(),
+	providerId: text('provider_id').notNull(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	accessToken: text('access_token').notNull(),
+	refreshToken: text('refresh_token'),
+	expiresAt: timestamp('expires_at', {
+		withTimezone: true,
+		mode: 'date'
+	})
+});
+
+export const emailVerificationTable = pgTable('email_verification', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	email: text('email').notNull(),
+	code: text('code').notNull(),
+	expiresAt: timestamp('expires_at', {
+		withTimezone: true,
+		mode: 'date'
+	}).notNull()
+});
+
+export const passwordResetTokenTable = pgTable('password_reset_token', {
+	tokenHash: text('token_hash').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp('expires_at', {
+		withTimezone: true,
+		mode: 'date'
+	}).notNull()
+});
+
+export const sessionTable = pgTable('session', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp('expires_at', {
+		withTimezone: true,
+		mode: 'date'
+	}).notNull()
+});
