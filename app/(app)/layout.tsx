80ca51b266ab48a210af838a60e6f1e71d957518
ch4dly <github.refute247@8alias.com>
2024-05-28 10:58:57 +0200

    Integrate request validation in Layout component

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ProfileContextProvider from '@/contexts/profile-context';
+import { validateRequest } from '@/lib/server-utils';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-	const data = {
+	const { user } = await validateRequest();
+
+	let data = {
 		user: {
 			name: '',
 			email: '',
@@ -10,6 +13,17 @@ export default async function Layout({ children }: { children: React.ReactNode }
 		},
 		links: []
 	};
+
+	if (user) {
+		data = {
+			user: {
+				name: user.name === null ? '' : user.name,
+				email: user.email === null ? '' : user.email,
+				profilePictureUrl: user.profilePictureUrl === null ? '' : user.profilePictureUrl
+			},
+			links: []
+		};
+	}
 
 	return <ProfileContextProvider data={data}>{children}</ProfileContextProvider>;
 }
