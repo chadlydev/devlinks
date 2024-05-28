8e99cf9b1bb5a9a75481e3e388704b348d186316
ch4dly <github.refute247@8alias.com>
2024-05-28 09:17:15 +0200

    Added Lucia authentication with Drizzle PostgreSQL adapter

---
+++ lib/lucia.ts
@@ -0,0 +1,41 @@
+import { Lucia } from 'lucia';
+import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
+import { db } from '@/db';
+import { sessionTable, userTable } from '@/db/schema';
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
+
+export const lucia = new Lucia(adapter, {
+	sessionCookie: {
+		// this sets cookies with super long expiration
+		// since Next.js doesn't allow Lucia to extend cookie expiration when rendering pages
+		expires: false,
+		attributes: {
+			// set to `true` when using HTTPS
+			secure: process.env.NODE_ENV === 'production'
+		}
+	},
+	getUserAttributes: (attributes) => {
+		return {
+			email: attributes.email,
+			emailVerified: attributes.emailVerified,
+			profilePictureUrl: attributes.profilePictureUrl,
+			name: attributes.name
+		};
+	}
+});
+
+// IMPORTANT!
+declare module 'lucia' {
+	interface Register {
+		Lucia: typeof lucia;
+		DatabaseUserAttributes: DatabaseUserAttributes;
+	}
+}
+
+type DatabaseUserAttributes = {
+	email: string;
+	emailVerified: boolean;
+	profilePictureUrl: string;
+	name: string;
+};
