332c933e06372968213c5cdc13aefb44fa51474b
ch4dly <github.refute247@8alias.com>
2024-05-28 11:29:14 +0200

    Added arctic package and oauth implementation

---
+++ lib/oauth.ts
@@ -0,0 +1,11 @@
+import { GitHub, Google } from 'arctic';
+
+export const github = new GitHub(process.env.GITHUB_CLIENT_ID!, process.env.GITHUB_CLIENT_SECRET!, {
+	redirectURI: process.env.NEXT_PUBLIC_APP_URL + '/api/oauth/github'
+});
+
+export const google = new Google(
+	process.env.GOOGLE_CLIENT_ID!,
+	process.env.GOOGLE_CLIENT_SECRET!,
+	process.env.NEXT_PUBLIC_APP_URL + '/api/oauth/google'
+);
