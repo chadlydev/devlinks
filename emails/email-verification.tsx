1b34a16a90f6000966cd788dfc04e48a6ff7df39
ch4dly <github.refute247@8alias.com>
2024-05-28 10:13:23 +0200

    Added email verification and password reset functionality

---
+++ emails/email-verification.tsx
@@ -0,0 +1,20 @@
+import { EMAIL_VERIFICATION_EXPIRES_IN } from '@/lib/constants';
+
+type EmailVerificationCodeProps = {
+	email: string;
+	code: string;
+};
+
+export default function EmailVerification({ email, code }: EmailVerificationCodeProps) {
+	return (
+		<div>
+			<p>Hello {email}, Thanks for signing up for Acme Inc.</p>
+			<p>Here&apos;s your 8-digit verification code: {code}</p>
+			<p>
+				This code expires in {EMAIL_VERIFICATION_EXPIRES_IN} minutes. Please enter it in the
+				application to complete your registration.
+			</p>
+			<p>Thank you, Acme Inc</p>
+		</div>
+	);
+}
---
+++ emails/password-reset.tsx
@@ -0,0 +1,17 @@
+import { RESET_PASSWORD_TOKEN_EXPIRES_IN } from '@/lib/constants';
+
+type PasswordResetProps = {
+	email: string;
+	url: string;
+};
+
+export default function PasswordReset({ email, url }: PasswordResetProps) {
+	return (
+		<div>
+			<p>Hello {email},</p>
+			<a href={url}>Click to reset your password</a>
+			<p>This link expires in {RESET_PASSWORD_TOKEN_EXPIRES_IN} minutes.</p>
+			<p>Thank you, Acme Inc</p>
+		</div>
+	);
+}
