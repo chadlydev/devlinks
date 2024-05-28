cbed8516415972d13d27dce863024e6ee96d89fe
ch4dly <github.refute247@8alias.com>
2024-05-28 11:00:45 +0200

    Ensure non-null profile picture URL in preview content

--- app/(app)/[username]/preview-content.tsx
+++ app/(app)/[username]/preview-content.tsx
@@ -14,7 +14,7 @@ export default function PreviewContent() {
 		<div className='md:bg-card z-20 flex w-full flex-grow flex-col items-center gap-12 overflow-x-clip whitespace-nowrap px-12 md:mx-auto md:w-96 md:rounded-2xl md:border md:px-8 md:py-12'>
 			<div className='flex flex-col items-center gap-5'>
 				<Avatar className='bg-card size-24 border'>
-					<AvatarImage src={user.profilePictureUrl} alt='avatar' />
+					<AvatarImage src={user.profilePictureUrl!} alt='avatar' />
 					<AvatarFallback className='text-muted-foreground'>
 						<AccountIcon size={48} />
 					</AvatarFallback>
