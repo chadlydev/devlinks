5bdac8a62a871d8e95d5ee4e718095322f59c7ef
ch4dly <github.refute247@8alias.com>
2024-05-29 09:50:26 +0200

    Refactor layout.tsx for user data retrieval

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
 import ProfileContextProvider from '@/contexts/profile-context';
 import { validateRequest } from '@/lib/server-utils';
+import { db } from '@/db';
+import { eq } from 'drizzle-orm';
+import { linkTable } from '@/db/schema';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
 	const { user } = await validateRequest();
 
-	let data = {
-		user: {
-			name: '',
-			email: '',
-			profilePictureUrl: ''
-		},
-		links: []
-	};
+	if (!user) return;
 
-	if (user) {
-		data = {
-			user: {
-				name: user.name ? user.name : '',
-				email: user.email ? user.email : '',
-				profilePictureUrl: user.profilePictureUrl ? user.profilePictureUrl : ''
-			},
-			links: []
-		};
-	}
+	const links = await db.query.linkTable.findMany({ where: eq(linkTable.userId, user.id) });
 
-	return <ProfileContextProvider data={data}>{children}</ProfileContextProvider>;
+	return <ProfileContextProvider data={{ user, links }}>{children}</ProfileContextProvider>;
 }
