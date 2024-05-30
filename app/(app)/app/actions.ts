50417f4466d531714e6e08281bbfc04ff98c9829
ch4dly <github.refute247@8alias.com>
2024-05-30 11:13:00 +0200

    Added email verification for email change request

--- app/(app)/app/actions.ts
+++ app/(app)/app/actions.ts
@@ -1,11 +1,20 @@
 'use server';
 
-import { argon2id, invalidateUserSessions, validateRequest } from '@/lib/server-utils';
-import { getErrorMessage } from '@/lib/utils';
-import { changeEmailFormSchema, changePasswordFormSchema } from '@/lib/zod';
-import { eq } from 'drizzle-orm';
+import {
+	argon2id,
+	createEmailVerificationCode,
+	invalidateUserSessions,
+	validateRequest
+} from '@/lib/server-utils';
+import { getErrorMessage, minutesToMs } from '@/lib/utils';
+import { changeEmailFormSchema, changePasswordFormSchema, verifyEmailFormSchema } from '@/lib/zod';
+import { eq, or } from 'drizzle-orm';
 import { db } from '@/db';
-import { userTable } from '@/db/schema';
+import { emailVerificationTable, userTable } from '@/db/schema';
+import { EMAIL_VERIFICATION_EXPIRES_IN, EMAIL_VERIFICATION_TIMER_MS } from '@/lib/constants';
+import { resend } from '@/emails';
+import EmailVerification from '@/emails/email-verification';
+import { isWithinExpirationDate } from 'oslo';
 
 export async function logoutAction() {
 	try {
@@ -82,7 +91,7 @@ export async function changePasswordAction(formData: unknown) {
 	}
 }
 
-export async function changeEmailAction(formData: unknown) {
+export async function requestEmailChangeAction(formData: unknown) {
 	// Validate formData
 	const validatedFormData = changeEmailFormSchema.safeParse(formData);
 	if (!validatedFormData.success) {
@@ -114,20 +123,121 @@ export async function changeEmailAction(formData: unknown) {
 
 		if (!validPassword) {
 			return {
-				error: 'Invalid password'
+				error: 'Invalid password',
+				type: 'invalid-password'
 			};
 		}
 
-		// Check for existing email change token, if there is one, return error
+		// check if email is available
+		const emailAlreadyTaken = await db.query.userTable.findFirst({
+			where: eq(userTable.email, email)
+		});
+		if (emailAlreadyTaken) {
+			return {
+				error: 'Email is already taken, please choose a different one',
+				type: 'email-not-available'
+			};
+		}
 
-		// send email to old & new email address, submit change when both verified the change?
+		// Check for existing email change token, if there is one, check the timer
+		const existingEmailVerification = await db.query.emailVerificationTable.findFirst({
+			where: or(eq(emailVerificationTable.email, email), eq(emailVerificationTable.userId, user.id))
+		});
+
+		if (existingEmailVerification) {
+			const createdAt =
+				existingEmailVerification.expiresAt.getTime() - minutesToMs(EMAIL_VERIFICATION_EXPIRES_IN);
+
+			if (createdAt > Date.now() - EMAIL_VERIFICATION_TIMER_MS) {
+				return {
+					error: 'Please wait before requesting another email change code'
+				};
+			}
+		}
+
+		// Delete any existing email change tokens
+		await db
+			.delete(emailVerificationTable)
+			.where(
+				or(eq(emailVerificationTable.email, email), eq(emailVerificationTable.userId, user.id))
+			);
+
+		// send email to new email address
+		const verificationCode = await createEmailVerificationCode(user.id, email);
+		await resend.emails.send({
+			from: `no-reply@${process.env.RESEND_DOMAIN}`,
+			to: [email],
+			subject: `Email Verification Code: ${verificationCode}`,
+			react: EmailVerification({ email: email, code: verificationCode })
+		});
 
 		return {
-			success: 'Password changed successfully'
+			success: 'Please check your inbox to verify your new email address'
 		};
 	} catch (error: unknown) {
 		return {
 			error: getErrorMessage(error)
+		};
+	}
+}
+
+export async function verifyEmailChangeAction(formData: unknown) {
+	// validate formData
+	const validatedFormData = verifyEmailFormSchema.safeParse(formData);
+	if (!validatedFormData.success) {
+		return {
+			error: 'Invalid data.'
+		};
+	}
+
+	// Extract code from validatedFormData
+	const { code } = validatedFormData.data;
+
+	try {
+		// Get current user details
+		const { user } = await validateRequest();
+		if (!user) {
+			return {
+				error: 'No user signed in'
+			};
+		}
+
+		const databaseCode = await db.query.emailVerificationTable.findFirst({
+			where: eq(emailVerificationTable.userId, user.id)
+		});
+
+		if (!databaseCode || databaseCode.code !== code) {
+			return {
+				error: 'Invalid verification code.'
+			};
+		}
+
+		await db.delete(emailVerificationTable).where(eq(emailVerificationTable.id, databaseCode.id));
+
+		if (!isWithinExpirationDate(databaseCode.expiresAt)) {
+			return {
+				error: 'Verification code is expired. Please try again.'
+			};
+		}
+
+		// Notify old email address that the email has been changed
+		await resend.emails.send({
+			from: `no-reply@${process.env.RESEND_DOMAIN}`,
+			to: [user.email],
+			subject: `Email address change notification`,
+			text: `Your email address has been changed to ${databaseCode.email}`
+		});
+
+		// change email
+		await db.update(userTable).set({ email: databaseCode.email }).where(eq(userTable.id, user.id));
+
+		return {
+			success: 'Email successfully changed'
+		};
+	} catch {
+		return {
+			error:
+				"The provided verification code is invalid. Please re-check and retry. If it's still not working, get-started a new code."
 		};
 	}
 }
--- app/(app)/app/change-email-form.tsx
+++ app/(app)/app/change-email-form.tsx
@@ -14,14 +14,30 @@ import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
-import { changeEmailFormSchema } from '@/lib/zod';
-import { changeEmailAction } from '@/app/(app)/app/actions';
+import { changeEmailFormSchema, verifyEmailFormSchema } from '@/lib/zod';
+import { requestEmailChangeAction, verifyEmailChangeAction } from '@/app/(app)/app/actions';
 import { toast } from 'sonner';
 import { useSessionContext } from '@/contexts/session-context';
+import { SetStateAction, useState } from 'react';
+import { Muted } from '@/components/typography';
+import { useRouter } from 'next/navigation';
+
+export default function ChangeEmailForm() {
+	const [showVerification, setShowVerification] = useState(false);
+
+	if (!showVerification) {
+		return <RequestEmailChangeForm setShowVerification={setShowVerification} />;
+	}
+	return <VerifyEmailChangeForm />;
+}
 
 type TChangeEmailForm = z.infer<typeof changeEmailFormSchema>;
 
-export default function ChangeEmailForm() {
+function RequestEmailChangeForm({
+	setShowVerification
+}: {
+	setShowVerification: (value: SetStateAction<boolean>) => void;
+}) {
 	const { user } = useSessionContext();
 
 	const form = useForm<TChangeEmailForm>({
@@ -40,13 +56,18 @@ export default function ChangeEmailForm() {
 	} = form;
 
 	const onSubmit = async (values: TChangeEmailForm) => {
-		const result = await changeEmailAction(values);
+		const result = await requestEmailChangeAction(values);
 
-		if (result.error) {
+		if (result.type === 'invalid-password') {
 			resetField('password');
 			setError('password', { message: result.error });
+		} else if (result.type === 'email-not-available') {
+			setError('email', { message: result.error });
+		} else if (result.error) {
+			toast.error(result.error);
 		} else if (result.success) {
 			toast.success(result.success);
+			setShowVerification(true);
 			reset();
 		}
 	};
@@ -88,6 +109,57 @@ export default function ChangeEmailForm() {
 				<Button variant='secondary' disabled={isSubmitting || !isDirty}>
 					Save changes
 				</Button>
+			</form>
+		</Form>
+	);
+}
+
+type TVerifyEmailChangeForm = z.infer<typeof verifyEmailFormSchema>;
+
+function VerifyEmailChangeForm() {
+	const router = useRouter();
+
+	const form = useForm<TVerifyEmailChangeForm>({
+		resolver: zodResolver(verifyEmailFormSchema),
+		defaultValues: {
+			code: ''
+		}
+	});
+
+	const {
+		reset,
+		formState: { isSubmitting, isDirty, isValid }
+	} = form;
+
+	const onSubmit = async (formData: TVerifyEmailChangeForm) => {
+		const result = await verifyEmailChangeAction(formData);
+		if (result.error) {
+			toast.error(result.error);
+			reset();
+		} else if (result.success) {
+			toast.success(result.success);
+			router.refresh();
+		}
+	};
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-4'>
+				<Muted>Enter your 8-digit code below to confirm your email change</Muted>
+				<FormField
+					control={form.control}
+					name='code'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Code</FormLabel>
+							<FormControl>
+								<FormInput type='number' placeholder='12345678' {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button disabled={isSubmitting || !isDirty || !isValid}>Submit</Button>
 			</form>
 		</Form>
 	);
