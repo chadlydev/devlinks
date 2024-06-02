cf0487ef7a9dde7884f526872753da3ce0ddb2da
ch4dly <github.refute247@8alias.com>
2024-06-02 15:08:27 +0200

    Update Header function to handle authenticated user

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -3,15 +3,18 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { ROUTE_LOGIN } from '@/lib/constants';
 import ThemeDropdownMenu from '@/components/theme-dropdown-menu';
+import { validateRequest } from '@/lib/server-utils';
 
-export default function Header() {
+export default async function Header() {
+	const { user } = await validateRequest();
+
 	return (
 		<header className='flex h-24 items-center justify-between p-4'>
 			<Logo />
 			<div className='flex items-center gap-4'>
 				<ThemeDropdownMenu />
 				<Button asChild>
-					<Link href={ROUTE_LOGIN}>Login</Link>
+					<Link href={ROUTE_LOGIN}>{!user ? 'Login' : 'Go to dashboard'}</Link>
 				</Button>
 			</div>
 		</header>
